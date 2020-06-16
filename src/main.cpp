16a6af8ec087b26d5f7892ff363790c79f954f4a
jackson211 <jackson21182@gmail.com>
2020-06-16 12:42:51 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -7,85 +7,13 @@
 #include <unordered_map>
 
 #include "../include/entry.h"
+#include "../include/index.h"
 #include "../include/linear.h"
 #include "../include/utils.h"
 
-template <typename T>
-std::unordered_map<T, int> countFreq(std::vector<T> &list) {
-  std::unordered_map<T, int> mp;
-
-  for (int i = 0; i < list.size(); i++)
-    mp[list[i]]++;
-
-  int total = 0;
-  int count = 0;
-  for (auto x : mp) {
-    std::cout << x.first << " " << x.second << std::endl;
-    total += x.second;
-    count++;
-  }
-  std::cout << "Total value: " << total << std::endl;
-  std::cout << "Average: " << total / count << std::endl;
-
-  return mp;
-}
-
-std::unordered_map<int, std::vector<int>>
-build_index_map(std::vector<int> &input) {
-  std::cout << "Build index map..." << std::endl;
-  std::unordered_map<int, std::vector<int>> index_map;
-  for (size_t i = 0; i < input.size(); i++) {
-    index_map[input[i]].push_back(i);
-  }
-
-  for (auto &item : index_map) {
-    std::cout << item.first << " -> ";
-    for (auto &i : item.second) {
-      std::cout << i << " ";
-    }
-    std::cout << std::endl;
-  }
-
-  return index_map;
-}
-
-template <typename Model, typename PredicateType = int>
-std::vector<int> look_up(std::vector<Entry *> &data, Model &model,
-                         std::unordered_map<int, std::vector<int>> &index_map,
-                         std::vector<long double> &test_set) {
-  std::cout << "Look up for test set..." << std::endl;
-  std::vector<int> test_result;
-
-  auto start = std::chrono::high_resolution_clock::now();
-
-  for (size_t k = 0; k < test_set.size(); k += 2) {
-    const long double test_lat = test_set[k];
-    const long double test_lon = test_set[k + 1];
-    int index = model.template predict<PredicateType>(test_lat);
-    int found = -1; // -1: not found
-    for (auto &i : index_map[index]) {
-      if (data.at(i)->getLon() == test_lon) {
-        if (data.at(i)->getLat() == test_lat) {
-          found = i;
-        }
-      }
-    }
-    test_result.push_back(found);
-  }
-  auto end = std::chrono::high_resolution_clock::now();
-  auto duration =
-      std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
-  std::cout << "Look up time: " << duration.count() << " nanoseconds"
-            << std::endl;
-  std::cout << "Average look up time: " << duration.count() / test_set.size()
-            << " nanoseconds" << std::endl;
-
-  return test_result;
-}
-
 int main(int argc, char *argv[]) {
   // Reading data from file
-  std::string filename = "test2.txt";
+  std::string filename = "test3.txt";
   std::vector<Entry *> data;
   bool sort_by_lat = utils::read_data<long double>(filename, &data);
   std::cout << "Size of data: " << sizeof(data.size()) << std::endl;
@@ -113,9 +41,6 @@ int main(int argc, char *argv[]) {
       *std::min_element(pred_result.begin(), pred_result.end());
   std::cout << "Max: " << MAX_PRED_VALUE << ", Min: " << MIN_PRED_VALUE
             << std::endl;
-  // for (size_t i = 0; i < pred_result.size(); i++) {
-  // pred_result[i] -= MIN_PRED_VALUE;
-  //}
   // std::unordered_map<int, int> index_freq = countFreq(pred_result);
 
   // Build test set
@@ -134,18 +59,20 @@ int main(int argc, char *argv[]) {
   test_y.push_back(-1);
 
   // Build indexes map
-  std::unordered_map<int, std::vector<int>> index_map =
-      build_index_map(pred_result);
+  std::unordered_map<int, std::vector<Entry *>> index_map =
+      learned_index::build_index_map(pred_result, data);
 
-  std::vector<int> test_result = look_up<model::Linear<long double>>(
-      data, linear_model, index_map, test_set);
+  std::vector<int> test_result =
+      learned_index::look_up<model::Linear<long double>>(linear_model,
+                                                         index_map, test_set);
 
   int correct = 0;
   for (int i = 0; i < test_result.size(); i++) {
     assert(test_result[i] == test_y[i]);
     correct++;
   }
-  std::cout << "Correct: " << correct << std::endl;
+  std::cout << "Correct: " << correct / test_result.size() * 100.0 << "% "
+            << std::endl;
 
   return 0;
 }
